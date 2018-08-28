4a0e044092471936b0a18cf03c372f1b3e2116b5
BusyBoy2 <38164004+BusyBoy2@users.noreply.github.com>
2018-08-29 01:15:05 +0530

    Add files via upload

---
+++ SparkMLIB.scala
@@ -0,0 +1,44 @@
+package com.spark.streaming
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object SparkMLIB {
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession
+      .builder()
+      .master("local")
+      .appName("Spark MLIB")
+      .config("spark.some.config.option", "some-value")
+      .getOrCreate()
+
+    println("Spark Session Object created")
+
+    // Removing all INFO logs in console printing only result sets
+    val rootLogger = Logger.getRootLogger()
+    rootLogger.setLevel(Level.ERROR)
+
+    val Flight = spark.read.format("CSV").option("header", true).load("C:\\Users\\lenovo\\Downloads\\DelayedFlights.csv")
+    val Fl = Flight.toDF()
+    // Flight.show()
+    Fl.registerTempTable("Flights_Table")
+    println("Flights_Table is registered!")
+
+    //Find out the top 5 most visited destinations.
+    val dest = spark.sql("""select Dest,count(dest) as Visits from Flights_Table  group by Dest """).toDF()
+    dest.sort(desc("Visits")).show(5)
+    println("Top 5 most visited destinations are as above!")
+
+    //Which month has seen the most number of cancellations due to bad weather?
+    val cancel = spark.sql("""select Month,count(FlightNum) as Num_Flights_Cancelled from Flights_Table WHERE Cancelled =1 AND CancellationCode="B" group by Month """).toDF()
+    cancel.show()
+    println("Month which has seen the most of the number of cancellations due to bad weather are as above!")
+
+    //Which route (origin & destination) has seen the maximum diversion?
+    val diversion = spark.sql("""select Origin,Dest,count(FlightNum) as Max_Diversion from Flights_Table where Diverted =1 group by Origin,Dest """)
+    diversion.toDF().sort(desc("Max_Diversion")).show()
+    println("List of Origin and destination that has seen the maximum diversion")
+  }
+}
